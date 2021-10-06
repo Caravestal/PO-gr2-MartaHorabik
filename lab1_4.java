592fffb39d4d090d4e70afd80f33a53121f597c8
Marta Horabik <58850165+Caravestal@users.noreply.github.com>
2021-10-06 18:05:51 +0200

    Create lab1_4.java

---
+++ lab1_4.java
@@ -0,0 +1,10 @@
+public class Start {
+    public static void main(String[] args){
+        int stan=1000;
+        for(int i=1; i<4; i++)
+        {
+           stan=stan/100*94;
+           System.out.println(stan);
+        }
+    }
+}
