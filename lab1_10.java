62077c709b64378d8c47bcd1e1600aa53ad3c528
Marta Horabik <58850165+Caravestal@users.noreply.github.com>
2021-10-06 18:43:35 +0200

    Create lab1_10.java

---
+++ lab1_10.java
@@ -0,0 +1,7 @@
+public class Start {
+    public static void main(String[] args){
+        System.out.println("film1");
+        System.out.println("film2");
+        System.out.println("film3");
+    }
+}
