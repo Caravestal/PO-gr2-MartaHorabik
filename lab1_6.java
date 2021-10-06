1999dcb53975afa6a1cc6c5c6ae66b53f1af1708
Marta Horabik <58850165+Caravestal@users.noreply.github.com>
2021-10-06 18:40:05 +0200

    Create lab1_6.java

---
+++ lab1_6.java
@@ -0,0 +1,11 @@
+public class Start {
+    public static void main(String[] args){
+        System.out.println("\u0f65");
+        System.out.println("\u13d2");
+        System.out.println("\u13ec");
+        System.out.println("\u13b6");
+        System.out.println("\u1587");
+        System.out.println("\u1560");
+        System.out.println("\u106e");
+    }
+}
